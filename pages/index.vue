a7f4af82a104c571ae81da5889166c5f2f71dae6
theweirdone <theweirdone@gmail.com>
2020-10-01 18:27:32 +0200

    updated a few more packages and fixed some formatting

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,19 @@
         <div class="columns is-mobile">
             <card title="Free" icon="github-circle">
                 Open source on
-                <a href="https://github.com/buefy/buefy">
-                    GitHub
-                </a>
+                <a href="https://github.com/buefy/buefy">GitHub</a>
             </card>
 
             <card title="Responsive" icon="cellphone-link">
-                <b class="has-text-grey">
-                    Every
-                </b>
+                <b class="has-text-grey">Every</b>
                 component is responsive
             </card>
 
             <card title="Modern" icon="alert-decagram">
                 Built with
-                <a href="https://vuejs.org/">
-                    Vue.js
-                </a>
+                <a href="https://vuejs.org/">Vue.js</a>
                 and
-                <a href="http://bulma.io/">
-                    Bulma
-                </a>
+                <a href="http://bulma.io/">Bulma</a>
             </card>
 
             <card title="Lightweight" icon="arrange-bring-to-front">
